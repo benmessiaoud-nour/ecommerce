464082620f33710cfccb5ec558c6bf7f6f6c0572
benmessiaoud nour <benmessiaoud.nour@example.com>
2024-07-24 22:16:06 +0200

    most sold product

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/style.css'
 import '@fortawesome/fontawesome-free/js/all.min.js';
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+document.querySelectorAll('.add-to-card-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert('اضيف هذا المنتج الي عربه الشراء');
+    })
+});
