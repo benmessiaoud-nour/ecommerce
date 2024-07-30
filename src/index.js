4947b76a4b64eb8185e08d8fed1d8d17a069e178
benmessiaoud nour <benmessiaoud.nour@example.com>
2024-07-31 00:30:10 +0200

    checkout page done

--- src/index.js
+++ src/index.js
@@ -25,6 +25,38 @@ document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
         })
         item.parentNode.parentNode.classList.add('active')
     })
+});
+
+// ----total price of the product-----
+document.querySelectorAll('[data-product-quantity]').forEach (item => {
+    item.addEventListener("change" , () => {
+        const newQuantity = item.value ;
+        const parent = item.closest ('[data-product-info]');
+        const pricePerUnit = parent.getAttribute('data-product-price');
+        const totalPriceForProduct = newQuantity * pricePerUnit;
+        parent.querySelector('.total-price-for-product').innerHTML = totalPriceForProduct + "$";
+        totalPriceForAllProducts();
+    })
 })
+
+
+document.querySelectorAll('[data-remove-from-card]').forEach(item =>{
+    item.addEventListener("click" , () =>{
+        item.closest('[data-product-info]').remove();
+        totalPriceForAllProducts();
+    })
+})
+
+function totalPriceForAllProducts() {
+    
+    let totalPriceForAllProducts = 0;
+    document.querySelectorAll('[data-product-info]').forEach (product => {
+        const pricePerUnite= product.getAttribute('data-product-price');
+        const quantity= product.querySelector('[data-product-quantity]').value;
+        const totalPriceForProduct= pricePerUnite * quantity;
+        totalPriceForAllProducts=  totalPriceForAllProducts +totalPriceForProduct;
+    })
+    document.getElementById('total-price-for-all-products').innerHTML= totalPriceForAllProducts + "$";
+}
 
 document.getElementById("copyright").innerHTML= "جميع الحقوق محفوظة" + new Date().getFullYear();
